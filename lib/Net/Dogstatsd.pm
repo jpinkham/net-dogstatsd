9ef999f11dd9f22e187b830bfb66ea0ee5790e80
Jennifer Pinkham <jpinkham@cpan.org>
2013-07-13 17:23:58 +0000

    Fixed license info to reflect GPL3

--- lib/Net/Dogstatsd.pm
+++ lib/Net/Dogstatsd.pm
@@ -738,11 +738,16 @@ partially developed on company time.
 
 Copyright 2013 Jennifer Pinkham.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the Artistic License.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3 as published by the Free
+Software Foundation.
 
-See http://dev.perl.org/licenses/ for more information.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
+You should have received a copy of the GNU General Public License along with
+this program. If not, see http://www.gnu.org/licenses/
 =cut
 
 1;
